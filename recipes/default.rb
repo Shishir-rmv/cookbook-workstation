c23b9023dd7525d4e70f30a2771aca77012eef25
vagrant <vagrant@localhost.localdomain>
2017-06-11 18:54:00 +0000

    initial workstation cookbook commit

---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: workstation
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
---
+++ recipes/setup.rb
@@ -0,0 +1,21 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntp' do
+  action :install
+end
+
+file '/etc/motd' do
+  content 'This server is the property of Shishir Verma'
+  owner 'root'
+  group 'root'
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end
+
+package 'git' do
+  action :install
+end
